6084bec7315bc68dd8a949c95889b1d9c1956dbf
micd84 <michal.s.debski@gmail.com>
2017-10-21 20:01:49 +0200

    Created basic project structure

---
+++ LanguageFeatures/Controllers/HomeController.cs
@@ -0,0 +1,24 @@
+﻿using LanguageFeatures.Models;
+using System.Web.Mvc;
+
+namespace LanguageFeatures.Controllers
+{
+    public class HomeController : Controller
+    {
+        public string Index()
+        {
+            return "Przejście do adresu URL pokazującego przykład";
+        }
+
+        public ViewResult AutoProperty()
+        {
+            var myProduct = new Product();
+
+            myProduct.Name = "Kajak";
+
+            string productName = myProduct.Name;
+
+            return View("Result",  (object)string.Format("Nazwa produktu to: {0}",productName));
+        }
+    }
+}
