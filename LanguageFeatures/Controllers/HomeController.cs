3ea0fa80385c650422ea5b1615e98cbd826c7e86
micd84 <michal.s.debski@gmail.com>
2017-10-28 15:54:49 +0200

    part 1

--- LanguageFeatures/Controllers/HomeController.cs
+++ LanguageFeatures/Controllers/HomeController.cs
@@ -1,5 +1,7 @@
 ﻿using LanguageFeatures.Models;
+using System.Collections.Generic;
 using System.Web.Mvc;
+using System.Text;
 
 namespace LanguageFeatures.Controllers
 {
@@ -18,7 +20,68 @@ namespace LanguageFeatures.Controllers
 
             string productName = myProduct.Name;
 
-            return View("Result",  (object)string.Format("Nazwa produktu to: {0}",productName));
+            return View("Result", (object)string.Format("Nazwa produktu: {0}", productName));
+        }
+
+        public ViewResult CreateCollection()
+        {
+            var cart = new ShoppingCart
+            {
+                Products = new List<Product>
+                {
+                    new Product {Name="Kajak",Price=275M},
+                    new Product {Name="Kamizelka ratunkowa",Price=48.95M},
+                    new Product {Name="Piłka nożna",Price=19.50M},
+                    new Product {Name="Flaga narożna",Price=34.95M}
+                }
+            };
+
+            decimal cartTotal = cart.TotalPrices();
+
+            return View("Result",
+                (object)string.Format("Razem: {0:c}", cartTotal));
+        }
+
+        public ViewResult UseExtensionEnumerable()
+        {
+            IEnumerable<Product> products = new ShoppingCart
+            {
+                Products = new List<Product>
+                {
+                    new Product {Name = "Kajak", Category = "Sporty wodne", Price = 275M},
+                    new Product {Name = "Kamizelka ratunkowa", Category = "Sporty wodne", Price = 48.95M},
+                    new Product {Name = "Piłka nożna", Category = "Piłka nożna", Price = 19.50M},
+                    new Product {Name = "Flaga narożna", Category = "Piłka nożna", Price = 34.95M}
+                }
+            };
+
+            decimal total = 0;
+
+            foreach (var prod in products.Filter(p => p.Category == "Piłka nożna"))
+            {
+                total += prod.Price;
+            }
+
+            return View("Result", (object)string.Format("Razem: {0}", total));
+        }
+
+        public ViewResult CreateAnonArray()
+        {
+            var oddsAndEnds = new[]
+            {
+                new { Name = "MVC", Category = "Wzorzec"},
+                new { Name = "Kapelusz", Category = "Odzież"},
+                new { Name = "Jabłko", Category = "Owoc"}
+            };
+
+            StringBuilder sb = new StringBuilder();
+
+            foreach (var item in oddsAndEnds)
+            {
+                sb.Append(item.Name).Append(" ");
+            }
+
+            return View("Result", (object)sb.ToString());
         }
     }
 }
---
+++ LanguageFeatures/Models/MyExtensionMethods.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LanguageFeatures.Models
+{
+    public static class MyExtensionMethods
+    {
+        public static decimal TotalPrices(this IEnumerable<Product> productEnum)
+        {
+            decimal total = 0;
+            foreach (Product prod in productEnum)
+            {
+                total += prod.Price;
+            }
+            return total;
+        }
+
+        public static IEnumerable<Product> FilterByCategory(
+            this IEnumerable<Product> productEnum, string categoryParam)
+        {
+            foreach (var prod in productEnum)
+            {
+                if (prod.Category == categoryParam)
+                {
+                    yield return prod;
+                }
+            }
+        }
+
+        public static IEnumerable<Product> Filter(
+            this IEnumerable<Product> prodEnum, Func<Product,bool> selectorParam)
+        {
+            foreach (var prod in prodEnum)
+            {
+                if (selectorParam(prod))
+                {
+                    yield return prod;
+                }
+            }
+        }
+    }
+}
